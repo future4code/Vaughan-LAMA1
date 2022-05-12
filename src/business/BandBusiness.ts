22ea37b3e8d3c53d2959284bb983336ade089da4
Grazielle Martins <fgagraziellemartins@hotmail.com>
2022-05-12 17:50:04 -0300

    Projeto Lama

---
+++ src/business/BandBusiness.ts
@@ -0,0 +1,24 @@
+// import { IdGenerator } from "../services/IdGenerator"
+// import { BandInputDTO } from "../model/Band"
+// import { HashManager } from "../services/HashManager";
+// import { BandDatabase } from "../data/BandDatabase";
+
+// export class BandBusiness {
+//         async createBand(band: BandInputDTO, token: string) {
+
+//                 const bandBusiness = new BandBusiness();
+//                 const newBand = await bandBusiness.
+
+
+//                 const idGenerator = new IdGenerator();
+//                 const id = idGenerator.generate();
+
+
+//                 const bandDatabase = new BandDatabase();
+//                 await bandDatabase.createBand(id, band.name, band.music_genre, band.responsible);
+
+//         }
+
+
+
+// }
---
+++ src/tests/mocks/userDatabaseMock.ts
@@ -0,0 +1,6 @@
+import {BaseDatabase} from "../../data/BaseDatabase";
+import { User } from "../../model/User";
+
+export class UserDatabaseMock{
+    public async getUserById(id: string): Promise<void> {}
+}
